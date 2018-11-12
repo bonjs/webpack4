036d0b58e0f460a188a53c2d3a8d6e8b01e33f9d
alex <observable@163.com>
2018-11-12 17:30:44 +0800

    模块文件异步加载

--- src/tpl/index.vue
+++ src/tpl/index.vue
@@ -15,9 +15,11 @@
 
 	Vue.use(VueRouter);
 	
-	import modeA from './modeA.vue';
-	import modeB from './modeB.vue';
+	//import modeA from './modeA.vue';
+	//import modeB from './modeB.vue';
 
+	const modeA = r => require.ensure([], () => r(require('./modeA.vue')), 'a')
+	const modeB = r => require.ensure([], () => r(require('./modeB.vue')), 'b')
 	
 	const routes = [
 		{ path: '/a', component: modeA },
